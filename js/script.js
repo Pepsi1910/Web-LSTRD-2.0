8e5f7c0175d38869c992eea9707ff10dd7a408c0
Fran Nguyen <dungmap96321@gmail.com>
2023-12-03 20:48:16 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function openLink(value) {
+    if (value == "logo") {
+        window.location.href = "index.html";
+    }
+    if (value == "thongtin") {
+        window.open ("https://gta5vn.com");
+    }
+    if (value == "gioithieu") {
+        window.open ("https://wiki.gta5vn.com/#/?_k=o0z4rz");
+    }
+    if (value == "bgd") {
+        window.open ("https://www.facebook.com/doicuuhogta5vn/?ref=page_internal");
+    }
+    if (value == "tp") {
+        window.location.href = "index.html";
+    }
+    if (value == "nvkc") {
+        window.location.href = "about.html";
+    }
+    if (value == "nvsn") {
+        window.location.href = "contact.html";
+    }
+    if (value == "luat") {
+        window.location.href = "imprint.html";
+    }
+    if (value == "dknv") {
+        window.location.href = "partner.html";
+    }
+    if (value == "dknvqs") {
+        window.location.href = "rule.html";
+    }
+    if (value == "dktp") {
+        //window.location.href = "error.html";
+        window.location.href = "register/nhan-vien/dieu-kien-lam-nhan-vien.html";
+    }
+    if (value == "huongdan") {
+        window.location.href = "error.html";
+        //window.location.href = "register/truong-phong/dieu-kien-lam-truong-phong.html";
+    }
+    if (value == "kiniem") {
+        window.location.href = "error.html";
+        //window.location.href = "register/nvqs/dieu-kien-di-nvqs.html";
+    }
+    if (value == "whitelist") {
+        alert('Tính năng chưa update !')
+    }
+}
+
